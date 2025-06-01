f04be472099ad8b0e11b7db23a2351dfd5d72a23
Gabriel Manica <gabrielfelipemanica@gmail.com>
2025-05-31 22:57:46 -0300

    style: altera a estrutura do título no componente Header para melhorar a acessibilidade e a usabilidade

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,12 @@ import { Link } from '@tanstack/react-router';
 export function Header() {
     return (
         <header className="fixed top-0 right-0 left-0 z-50 border-b border-gray-200 bg-white/40 px-4 py-3 backdrop-blur-md">
-            <Link to="/" className="font-inter text-2xl font-extralight">
+            <h1 className="font-inter text-2xl font-extralight cursor-default select-none">
                 Robodoc{' '}
-                <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent font-semibold">
+                <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text font-semibold text-transparent">
                     Start
                 </span>
-            </Link>
+            </h1>
         </header>
     );
 }
